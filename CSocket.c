bb4003f9178a839db23e50fdca68bdf34d01f087
crossddelta <danilo.lessa@aluno.ifsp.edu.br>
2022-08-16 01:10:51 -0300

    Nome adicionado, lógica e funcionamento aprimorados

--- CSocket.c
+++ CSocket.c
@@ -1,29 +1,46 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
-int main(void){
+int main(int argc, char *argv[]){
     
 	int meusocket;
 	int conecta;
 
-	struct sockaddr_in alvo;
+	printf("===================================================\n");
+	printf("+                  C S O C K E T                  +\n");
+	printf("===================================================\n");
 
-   	meusocket = socket(AF_INET, SOCK_STREAM, 0);
-    	alvo.sin_family = AF_INET;
-    	alvo.sin_port = htons(80);
-    	alvo.sin_addr.s_addr = inet_addr("192.168.0.1");
+	if(argv[1] == NULL || argv[2] == NULL){
+			printf("Informe um endereco IP e uma porta\npara verificar se está aberta\n");
+			printf("Modo de uso: ./CResolver 192.168.0.1 80\n");
 
-    	conecta = connect(meusocket, (struct sockaddr *)&alvo, sizeof alvo);
+			return(0);
+	}
+	else{
+		int port = strtol(argv[2], NULL, 10);	
+		char *ip = argv[1];
 
-    	if(conecta == 0){
-        	printf("Porta aberta!\n");
-        	close(meusocket);
-        	close(conecta);
-    	}
-    	else{
-        	printf("Porta fechada!\n");
-    	}
+		struct sockaddr_in alvo;
 
+   		meusocket = socket(AF_INET, SOCK_STREAM, 0);
+    		alvo.sin_family = AF_INET;
+    		alvo.sin_port = htons(port);
+    		alvo.sin_addr.s_addr = inet_addr(ip);
+
+    		conecta = connect(meusocket, (struct sockaddr *)&alvo, sizeof alvo);
+
+    		if(conecta == 0){
+        		printf("Porta aberta!\n");
+        		close(meusocket);
+        		close(conecta);
+    		}
+    		else{
+        		printf("Porta fechada!\n");
+    		}
+	}
     return(0);
 }
